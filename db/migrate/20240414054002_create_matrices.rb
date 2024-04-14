9e9ca7f98af2d6bcb388fd09d3eddb0a94122735
Sangamesh B M <sangamesh71998@gmail.com>
2024-04-14 17:59:21 +0530

    Final commit

--- db/migrate/20240414054002_create_matrices.rb
+++ db/migrate/20240414054002_create_matrices.rb
@@ -1,7 +1,7 @@
 class CreateMatrices < ActiveRecord::Migration[7.0]
   def change
     create_table :matrices do |t|
-
+      t.integer :data, array: true, null: false
       t.timestamps
     end
   end
